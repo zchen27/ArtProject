b9908a09d867d5a9da5fc0af8e540c7f57174d1e
zchen27 <zchen27@gmail.com>
2014-01-02 14:08:06 -0500

    Commit

---
+++ src/Mutation.java
@@ -0,0 +1,12 @@
+
+public abstract class Mutation
+{
+	public abstract double widenLight();
+	public abstract double widenHeat();
+	public abstract double widenHumidity();
+	public abstract double widenpH();
+	public abstract double changeLight();
+	public abstract double changeHeat();
+	public abstract double changeHumidity();
+	public abstract double changepH();
+}
