94423635269bd0807b15a21d925229300052146f
zchen27 <zchen27@gmail.com>
2014-01-07 14:09:12 -0500

    Commit

---
+++ src/Address.java
@@ -0,0 +1,18 @@
+
+public class Address
+{
+	public final int AD;
+	public final AddressMode MODE;
+	
+	Address(int ad, AddressMode mode)
+	{
+		AD = ad;
+		MODE = mode;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return (AD + " " + MODE.MODE);
+	}
+}
