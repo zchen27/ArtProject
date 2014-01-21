359efee7cf5a3ce6dd40d8bfde4e25ac85bb6d06
zchen27 <zchen27@gmail.com>
2014-01-20 23:11:46 -0500

    Revised Register Mechanics

--- src/Core.java
+++ src/Core.java
@@ -1,7 +1,41 @@
 import java.util.*;
+import java.util.concurrent.*;
 public class Core
 {
-	private ArrayList<Instruction> mars = new ArrayList(8000);
-	private ArrayList<ArrayList<Integer>> pointers;
+	private class Pointer implements Runnable
+	{
+		private int location;
+		private boolean alive = true;;
+		@Override
+		public void run()
+		{
+			while (alive)
+			{
+				
+			}
+		}
+		
+		public void execute() throws UnsupportedOperationException
+		{
+			Register register = mars[location];
+			if(register == null)
+			{
+				alive = false;
+			}
+		}
+		
+		public void increment()
+		{
+			location++;
+		}
+		
+		public void jump(int distance)
+		{
+			location += distance;
+		}
+	}
+	
+	private volatile Register[] mars = new Register[8000];
+	private ConcurrentNavigableMap<String, Runnable> pointers;
 	
 }
--- src/Register.java
+++ src/Register.java
@@ -1,14 +1,102 @@
 
 public class Register
 {
-	public final Instruction instruction;
-	public final Address aSpace;
-	public final Address bSpace;
-	
-	Register(Instruction op, Address a, Address b)
+	public enum Opcode
 	{
-		instruction = op;
-		aSpace = a;
-		bSpace = b;
+		DAT,
+		MOV,
+		ADD,
+		SUB,
+		MUL,
+		DIV,
+		MOD,
+		JMP,
+		JMZ,
+		JMN,
+		DJN,
+		SPL,
+		SEQ,
+		SNE,
+		CMP,
+		SLT,
+		NOP,
+	}
+	
+	public enum Modifier
+	{
+		A,
+		B,
+		AB,
+		BA,
+		F,
+		X,
+		I,
+	}
+	
+	public enum Mode
+	{
+		IMM,
+		DIR,
+		AID,
+		BID,
+		AIP,
+		BIP,
+		AIO,
+		BIO,
+	}
+	
+	private Opcode op;
+	private Modifier mod;
+	private Mode aMode;
+	private Mode bMode;
+	private int a;
+	private int b;
+	
+	public Opcode getop()
+	{
+		return op;
+	}
+	
+	public Modifier getMod()
+	{
+		return mod;
+	}
+	
+	public int getA()
+	{
+		return a;
+	}
+	
+	public int getB()
+	{
+		return b;
+	}
+	
+	public Mode getAMode()
+	{
+		return aMode;
+	}
+	
+	public Mode getBMode()
+	{
+		return bMode;
+	}
+	
+	public void setOp(Opcode newOp, Modifier newMod)
+	{
+		op = newOp;
+		mod = newMod;
+	}
+	
+	public void setA(Mode newAMode, int newA)
+	{
+		aMode = newAMode;
+		a = newA;
+	}
+	
+	public void setB(Mode newBMode, int newB)
+	{
+		bMode = newBMode;
+		b = newB;
 	}
 }
