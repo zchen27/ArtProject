daad6b74ba35548ceef98d80627dcab65efdde02
zchen27 <zchen27@gmail.com>
2014-01-23 14:08:47 -0500

    Recommit

--- src/Mars.java
+++ src/Mars.java
@@ -2,5 +2,19 @@
 public class Mars
 {
 	public Register[] core;
+	public final int CORESIZE;
+	public final int MAXCYCLES;
+	public final int WARRIORS;
+	public final int MAXLENGTH;
+	public final int MINDISTANCE;
 	
+	Mars(int coresize, int warriors, int maxcycles, int maxlength, int mindistance)
+	{
+		CORESIZE = coresize;
+		core = new Register[coresize];
+		WARRIORS = warriors;
+		MAXCYCLES = maxcycles;
+		MAXLENGTH = maxlength;
+		MINDISTANCE = mindistance;
+	}
 }
