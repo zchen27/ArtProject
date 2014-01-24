a9f62c10df7bedd769c15979af5b4f0311753c6d
zchen27 <zchen27@gmail.com>
2014-01-24 00:01:35 -0500

    Commit

--- src/Mars.java
+++ src/Mars.java
@@ -17,4 +17,6 @@ public class Mars
 		MAXLENGTH = maxlength;
 		MINDISTANCE = mindistance;
 	}
+        
+        
 }
--- src/WarriorRun.java
+++ src/WarriorRun.java
@@ -1,6 +1,9 @@
 
 public class WarriorRun
 {
-	public int pointer;
-	
+	private int pointer;
+	private WarriorRun next;
+        private WarriorRun previous;
+        private Warrior war;
+        
 }
